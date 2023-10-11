4c04561c9d3682f272ff84d99cb678700d0f4089
Pablo Costa <pablo@botsfactory.io>
2023-10-11 14:50:00 -0300

    Refactor `list.sh` code

--- bin/list.sh
+++ bin/list.sh
@@ -7,39 +7,19 @@ CWD=`/usr/bin/dirname $0`
 cd $CWD
 . ./functions.sh
 
-# check parameters
-if [ "x${TARGET_PRIVATE_BUCKET_URL}" == "x" ]; then
-  echo "ERROR: The environment variable TARGET_PRIVATE_BUCKET_URL must be specified." 1>&2
-  exit 1
-fi
-
-TARGET_BUCKET_URL=${TARGET_PRIVATE_BUCKET_URL}
-
-# output final file list
-if [ `echo $TARGET_BUCKET_URL | cut -f1 -d":"` == "s3" ]; then
-  echo "There are files below in '${TARGET_BUCKET_URL}' S3 bucket:"
-  s3_list_files ${TARGET_BUCKET_URL}
-elif [ `echo $TARGET_BUCKET_URL | cut -f1 -d":"` == "gs" ]; then
-  echo "There are files below in '${TARGET_BUCKET_URL}' GS bucket:"
-  gs_list_files ${TARGET_BUCKET_URL}
-fi
-
-# check parameters
-if [ "x${TARGET_PUBLIC_BUCKET_URL}" == "x" ]; then
-  echo "ERROR: The environment variable TARGET_PUBLIC_BUCKET_URL must be specified." 1>&2
-  exit 1
-fi
-
-TARGET_BUCKET_URL=${TARGET_PUBLIC_BUCKET_URL}
-
-# output final file list
-if [ `echo $TARGET_BUCKET_URL | cut -f1 -d":"` == "s3" ]; then
-  echo "There are files below in '${TARGET_BUCKET_URL}' S3 bucket:"
-  s3_list_files ${TARGET_BUCKET_URL}
-elif [ `echo $TARGET_BUCKET_URL | cut -f1 -d":"` == "gs" ]; then
-  echo "There are files below in '${TARGET_BUCKET_URL}' GS bucket:"
-  gs_list_files ${TARGET_BUCKET_URL}
-fi
+for TARGET_BUCKET_URL in ${TARGET_PRIVATE_BUCKET_URL} ${TARGET_PUBLIC_BUCKET_URL}
+do
+  if [ "x${TARGET_BUCKET_URL}" != "x" ]; then
+    # output final file list
+    if [ `echo $TARGET_BUCKET_URL | cut -f1 -d":"` == "s3" ]; then
+      echo "There are files below in '${TARGET_BUCKET_URL}' S3 bucket:"
+      s3_list_files ${TARGET_BUCKET_URL}
+    elif [ `echo $TARGET_BUCKET_URL | cut -f1 -d":"` == "gs" ]; then
+      echo "There are files below in '${TARGET_BUCKET_URL}' GS bucket:"
+      gs_list_files ${TARGET_BUCKET_URL}
+    fi
+  fi
+done
 
 # output Cludflare R2 account bucket file list
 if [ "x${CLOUDFLARE_ACCOUNT_ID}" != "x" ]; then
